210e43bf59c2ed85a18cbd9e61bb48f152d8ea75
sharky101 <dan.howitt@iondrivesoftware.com>
2014-06-09 16:12:45 +0100

    build: browserify bundle

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-core
-====
+# @coinative/core
 
-Bitcoin Core Library
+Bitcoin core library for NodeJS and browsers.
+
+## Install
+
+Not currently hosted on npmjs.org. Take this module as a git dependency via:
+
+```
+npm install coinative/core-mnemonic
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  base58: require('core-base58'),
+  hash: require('core-hash'),
+  Address: require('core-address'),
+  HdKey: require('core-hdkey'),
+  Key: require('core-key'),
+  mnemonic: require('core-mnemonic'),
+  MsKey: require('core-mskey'),
+  Script: require('core-script'),
+  sjcl: require('core-sjcl')
+};
